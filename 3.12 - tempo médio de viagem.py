7ab028d9d2fd079843e435c9febe6992bbbd98cb
Davi Lopes <48262612+davi-ls@users.noreply.github.com>
2022-06-19 12:48:00 -0300

    Delete 3.12 - tempo médio de viagem.py

--- 3.12 - tempo médio de viagem.py
+++
@@ -1,5 +0,0 @@
-distancia = int(input("Digite a distância a ser percorrida em KM: "))
-velocidade_media = int(input("Digite a velocidade média (horas): "))
-
-tempo_viagem = distancia/velocidade_media
-print(f"O tempo de viagem é de {tempo_viagem} horas")
